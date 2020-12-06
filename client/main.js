b7f490f18bf81712b57ae58189294a9367f78aa8
Eliot Uriarte <ueliot@yahoo.com>
2020-12-06 22:25:48 +0100

    problem in path

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import Vue from 'vue';
- 
+// Problem no load this module this path was errate
 import App from '../imports/ui/App.vue';
  
 Meteor.startup(() => {
